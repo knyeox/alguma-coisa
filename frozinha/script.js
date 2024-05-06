3b304c56917029fdb6d0f233e67fdfe31b4ff656
Joshua <125416629+knyeox@users.noreply.github.com>
2024-05-06 16:56:59 -0300

    Add files via upload

---
+++ frozinha/script.js
@@ -0,0 +1,7 @@
+gsap.to(".rose", {
+    duration: 2,
+    scale: 1,
+    ease: "elastic.out(1, 0.3)", // Efeito elástico para uma animação mais realista
+    stagger: 0.5 // Atraso entre as animações das rosas
+  });
+  
